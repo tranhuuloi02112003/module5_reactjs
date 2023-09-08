3e4943e5491d20d9f8eb16f7f703581e7c5eb2bc
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2023-09-08 19:04:53 +0700

    up

---
+++ fibonacci-sum/fibonacci-sum.ts
@@ -0,0 +1,15 @@
+let array = [0, 1];
+let sumFibonacci = 1;
+let fibonacci=(number)=>{
+    if (number <= 1) {
+        return 1;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+for (let i = 1; i <= 9; i++) {
+    array.push(fibonacci(i));
+    sumFibonacci += fibonacci(i);
+}
+console.log(array.toString());
+console.log("Tổng dãy số fibonacci:")
+
